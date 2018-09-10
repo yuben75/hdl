2c9da19ca2e0ecb5d545779b004ee6591f4a64a5
Istvan Csomortani <istvan.csomortani@analog.com>
2018-09-10 10:06:26 +0100

    ad40xx/zed: Use a separate clock for SCLK generation

--- projects/ad40xx_fmc/common/ad40xx_bd.tcl
+++ projects/ad40xx_fmc/common/ad40xx_bd.tcl
@@ -1,6 +1,12 @@
 
 create_bd_intf_port -mode Master -vlnv analog.com:interface:spi_master_rtl:1.0 ad40xx_spi
 
+
+## To support the 1.8MSPS (SCLK == 100 MHz), set the spi clock to 200 MHz
+set_property -dict [list \
+   CONFIG.PCW_EN_CLK2_PORT {1} \
+   CONFIG.PCW_FPGA2_PERIPHERAL_FREQMHZ $spi_clk_ref_frequency] [get_bd_cells sys_ps7]
+
 # create a SPI Engine architecture
 
 create_bd_cell -type hier spi_ad40xx
@@ -8,6 +14,7 @@ current_bd_instance /spi_ad40xx
 
   create_bd_pin -dir I -type clk clk
   create_bd_pin -dir I -type rst resetn
+  create_bd_pin -dir I -type clk spi_clk
   create_bd_intf_pin -mode Master -vlnv analog.com:interface:spi_master_rtl:1.0 m_spi
   create_bd_intf_pin -mode Master -vlnv xilinx.com:interface:axis_rtl:1.0 M_AXIS_SAMPLE
 
@@ -20,9 +27,11 @@ current_bd_instance /spi_ad40xx
   ad_ip_instance axi_spi_engine axi
   ad_ip_parameter axi CONFIG.DATA_WIDTH $adc_resolution
   ad_ip_parameter axi CONFIG.NUM_OFFLOAD 1
+  ad_ip_parameter axi CONFIG.ASYNC_SPI_CLK 1
 
   ad_ip_instance spi_engine_offload offload
   ad_ip_parameter offload CONFIG.DATA_WIDTH $adc_resolution
+  ad_ip_parameter offload CONFIG.ASYNC_SPI_CLK 1
 
   ad_ip_instance spi_engine_interconnect interconnect
   ad_ip_parameter interconnect CONFIG.DATA_WIDTH $adc_resolution
@@ -31,9 +40,7 @@ current_bd_instance /spi_ad40xx
 
   ## to setup the sample rate of the system change the PULSE_PERIOD value
   ## the acutal sample rate will be PULSE_PERIOD * (1/sys_cpu_clk)
-  ## fsys_cpu_clk must be 100 MHz
-  set cycle_per_sec_100mhz 100000000
-  set sampling_cycle [expr int(ceil(double($cycle_per_sec_100mhz) / $adc_sampling_rate))]
+  set sampling_cycle [expr int(ceil(double($spi_clk_ref_frequency * 1000000) / $adc_sampling_rate))]
   ad_ip_parameter trigger_gen CONFIG.PULSE_PERIOD $sampling_cycle
   ad_ip_parameter trigger_gen CONFIG.PULSE_WIDTH 1
 
@@ -56,7 +63,7 @@ current_bd_instance /spi_ad40xx
     ad_connect offload/offload_sdi axis_upscaler/s_axis
     ad_connect axis_upscaler/m_axis M_AXIS_SAMPLE
 
-    ad_connect clk axis_upscaler/clk
+    ad_connect spi_clk axis_upscaler/clk
     ad_connect axi/spi_resetn axis_upscaler/resetn
   } else {
     ad_connect offload/offload_sdi M_AXIS_SAMPLE
@@ -64,13 +71,13 @@ current_bd_instance /spi_ad40xx
 
   ad_connect execution/spi m_spi
 
-  ad_connect clk offload/spi_clk
+  ad_connect spi_clk offload/spi_clk
   ad_connect clk offload/ctrl_clk
-  ad_connect clk execution/clk
+  ad_connect spi_clk execution/clk
   ad_connect clk axi/s_axi_aclk
-  ad_connect clk axi/spi_clk
-  ad_connect clk interconnect/clk
-  ad_connect clk trigger_gen/clk
+  ad_connect spi_clk axi/spi_clk
+  ad_connect spi_clk interconnect/clk
+  ad_connect spi_clk trigger_gen/clk
 
   ad_connect axi/spi_resetn offload/spi_resetn
   ad_connect axi/spi_resetn execution/resetn
@@ -84,6 +91,9 @@ current_bd_instance /spi_ad40xx
   ad_connect resetn axi/s_axi_aresetn
 
 current_bd_instance /
+
+# asynchronous SPI clock, to support higher SCLK
+ad_connect spi_clk sys_ps7/FCLK_CLK2
 
 # dma to receive data stream
 
@@ -104,9 +114,11 @@ if {$adc_resolution != 16} {
 ad_ip_parameter axi_ad40xx_dma CONFIG.DMA_DATA_WIDTH_DEST 64
 
 ad_connect  sys_cpu_clk spi_ad40xx/clk
-ad_connect  sys_cpu_clk axi_ad40xx_dma/s_axis_aclk
+ad_connect  spi_clk axi_ad40xx_dma/s_axis_aclk
 ad_connect  sys_cpu_resetn spi_ad40xx/resetn
 ad_connect  sys_cpu_resetn axi_ad40xx_dma/m_dest_axi_aresetn
+
+ad_connect  spi_clk spi_ad40xx/spi_clk
 
 ad_connect  spi_ad40xx/m_spi ad40xx_spi
 ad_connect  axi_ad40xx_dma/s_axis spi_ad40xx/M_AXIS_SAMPLE
